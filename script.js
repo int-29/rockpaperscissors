f2e674502708271470320ef4b06b2e559deb0754
Ion Moraru <moraru.ion767@gmail.com>
2025-04-11 14:26:44 +0300

    Bug

--- script.js
+++ script.js
@@ -11,11 +11,6 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt('Enter Rock Paper or Scissors');
-    console.log('Your choice: ', humanChoice);
-    return humanChoice.toLowerCase();
-}
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'Scissors') {
@@ -46,25 +41,89 @@ function playRound(humanChoice, computerChoice) {
 let humanScore = 0;
 let computerScore = 0;
 
-function playGame() {
-    for (let i = 1; i <=5; i++) {
-        console.log(`Round ${i}`);
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
+// let humanSelection = getHumanChoice();
+// let computerSelection = getComputerChoice();
 
-    console.log('Final Result:');
-    console.log('You: ', humanScore);
-    console.log('Computer', computerScore);
+const container = document.querySelector('#container');
+const button_rock = document.createElement('button');
+button_rock.setAttribute('style', 'width:100px; height: 50px; margin-right: 10px;');
+button_rock.textContent = 'Rock';
+container.appendChild(button_rock);
 
-    if (computerScore > humanScore) {
-        console.log('The Computer Wins!');
-    } else if (humanScore > computerScore) {
-        console.log('You Win!');
-    } else {
-        console.log("It's a draw!")
-    }
+const button_paper = document.createElement('button');
+button_paper.setAttribute('style', 'width:100px; height: 50px; margin-right: 10px;');
+button_paper.textContent = 'Paper';
+container.appendChild(button_paper);
+
+const button_scissors = document.createElement('button');
+button_scissors.setAttribute('style', 'width:100px; height: 50px');
+button_scissors.textContent = 'Scissors';
+container.appendChild(button_scissors);
+
+const results = document.querySelector('#results');
+const humanResult = document.createElement('p');
+humanResult.textContent = 'You: ' + humanScore;
+results.appendChild(humanResult);
+
+
+const computerResult = document.createElement('p');
+computerResult.textContent = 'Computer: ' + humanScore;
+results.appendChild(computerResult);
+
+const winner = document.createElement('p');
+winner.textContent = 'Winner: ';
+results.appendChild(winner);
+
+function getHumanChoice() {
+    let choice;
+    button_rock.addEventListener('click', () => {
+        // playRound('rock', computerSelection);
+        choice = 'rock';
+    })
+    
+    button_paper.addEventListener('click', () => {
+        // playRound('paper', computerSelection);
+        choice = 'paper';
+    })
+    
+    button_scissors.addEventListener('click', () => {
+        // playRound('scissors', computerSelection);
+        choice = 'scissors';
+    })
+    return choice;
+//     // let humanChoice = prompt('Enter Rock Paper or Scissors');
+//     // console.log('Your choice: ', humanChoice);
+//     // return humanChoice.toLowerCase();
 }
 
-playGame()
+for (let round = 1; round <= 5; round++) {
+    let humanSelection = getHumanChoice();
+    let computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+}
+
+
+
+
+// function playGame() {
+//     for (let i = 1; i <=5; i++) {
+//         console.log(`Round ${i}`);
+//         let humanSelection = getHumanChoice();
+//         let computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     }
+
+//     console.log('Final Result:');
+//     console.log('You: ', humanScore);
+//     console.log('Computer', computerScore);
+
+//     if (computerScore > humanScore) {
+//         console.log('The Computer Wins!');
+//     } else if (humanScore > computerScore) {
+//         console.log('You Win!');
+//     } else {
+//         console.log("It's a draw!");
+//     }
+// }
+
+// playGame()
